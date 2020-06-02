d08435f4b2b18e46360fa348c8ca2dd077e509e8
Evan Wilbur <ewilbur9@gmail.com>
2020-06-02 17:07:57 -0500

    Cheeky way of doing it in one line

--- assignment-1/exercise-4.c
+++ assignment-1/exercise-4.c
@@ -10,11 +10,9 @@
 
 int main(void) {
     int i = 0;
-    for (; i < 3; ++i, printf("For he's a jolly good fellow!\n"))
+    for (; i < 3 || (!printf("Which nobody can deny!\n")); ++i, printf("For he's a jolly good fellow!\n"))
         ;
-    printf("Which nobody can deny!\n");
-
    printf("Press any character to continue");
    getchar();
-    return 0;
+   return 0;
 }
