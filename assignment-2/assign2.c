7a9ca2ea80999cd0984ec65a6731d3335940e1b1
Evan Wilbur <ewilbur9@gmail.com>
2020-06-03 14:24:55 -0500

    Create skeleton for assignment 2

---
+++ assignment-2/assign2.c
@@ -0,0 +1,60 @@
+/*
+  Assignment #2
+  CSCI 1111-10
+
+  Complete the functions to solve
+  problems 2, 3, 6, & 7 on pages 97 
+  from the Prata book. 
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void page97_2(void);
+void page97_3(void);
+void page97_6(void);
+void page97_7(void);
+
+
+int main()
+{
+    printf("Solution to page 97 #2\n");
+    page97_2();
+
+    printf("Solution to page 97 #3\n");
+    page97_3();
+    
+    printf("Solution to page 97 #6\n");
+    page97_6();
+
+    printf("Solution to page 97 #7\n");
+    page97_7();
+
+    system("pause"); //dos specific
+    return 0;
+
+}
+
+void page97_2(void)
+{
+     printf("Under Construction\n\n");
+
+}
+
+void page97_3(void)
+{
+     printf("Under Construction\n\n");
+}
+
+void page97_6(void)
+{
+     printf("Under Construction\n\n");
+}
+
+void page97_7(void)
+{
+     printf("Under Construction\n\n");
+
+}
