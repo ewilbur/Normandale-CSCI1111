95483390c9b83ef53901bcbcc8d456c4c9d6f72b
Evan Wilbur <ewilbur9@gmail.com>
2020-06-06 12:21:02 -0500

    Created assignment-3 folder and initialized with skeleton

---
+++ assignment-3/assignment3.c
@@ -0,0 +1,68 @@
+/*
+  Assignment #3
+  CSCI 1111-10
+
+  Complete the functions to solve
+  problems 2, 3, 4, and 7 pages 140, 141,
+  from the Prata book. 
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void page141_2(void);
+void page141_3(void);
+void page141_4(void);
+void page141_6(void);
+void page141_7(void);
+
+
+int main()
+{
+    printf("Solution to page 141 #2\n");
+    page97_2();
+
+    printf("Solution to page 141 #3\n");
+    page97_3();
+    
+    printf("Solution to page 141 #4\n");
+    page97_4();
+    
+    printf("Solution to page 141 #6\n");
+    page97_6();
+
+    printf("Solution to page 141 #7\n");
+    page97_7();
+
+    system("pause"); //dos specific
+    return 0;
+
+}
+
+void page141_2(void)
+{
+     printf("Under Construction\n\n");
+
+}
+
+void page141_3(void)
+{
+     printf("Under Construction\n\n");
+}
+
+void page141_4(void)
+{
+     printf("Under Construction\n\n");
+}
+
+void page141_6(void)
+{
+     printf("Under Construction\n\n");
+}
+
+void page141_7(void)
+{
+     printf("Under Construction\n\n");
+}
