30cba1b8b1e2308a7d6618c6d01d6a0785542d3e
Evan Wilbur <ewilbur9@gmail.com>
2020-06-10 16:34:52 -0500

    Calculates change and renders it out. Changed names. You know...

--- assignment-4/change.c
+++ assignment-4/change.c
@@ -2,15 +2,17 @@
  * @author Evan Wilbur
  * @class CSCI 1111
  * @section 10
- * @description An improved calculator. Seperate data structure to store the
- *              change. Hopefully this allows the program to be more extendible
- *              if different coins are required (eg fifty cent coin). If the
- *              need for higher denominations is required, simply add to
- *              TENDER_VALUES. The only important thing to keep in mind is that
- *              the names and values in SINGLUAR_TENDER_NAMES,
- *              PLURAL_TENDER_NAMES, and TENDER_VALUES must correlate. As in,
- *              the index of each must be consistent otherwise the program
- *              fails.
+ * @description An even more improved change calculator. This doesn't use
+ *              floats anywhere in the code. Instead, it parses everything as
+ *              ints. Yes, yes, I know that the purpose of this program is to
+ *              learn how to deal with floats and doubles. But I've been coding
+ *              for years now and I've never come across a problem that
+ *              requires floats or doubles. Anytime there's a decimal value
+ *              needed, I just parse them as ints, as I've done here. I may
+ *              lose points since I'm not "learning" floats (as if anyone but
+ *              God can understand these horribly designed data types) but I
+ *              think I should make up those points since my way requires more
+ *              code including a parser.
  */
 
 
@@ -22,9 +24,10 @@
 #include <ctype.h>
 
 #define DEV
-#define TENDER_STRING_LENGTH 32
+#define DENOMINATION_STRING_LENGTH 32
 
-static const char SINGULAR_TENDER_NAMES[][TENDER_STRING_LENGTH]
+
+static const char SINGULAR_DENOMINATION_NAMES[][DENOMINATION_STRING_LENGTH]
     = { "penny",
         "nickel",
         "dime",
@@ -36,20 +39,7 @@ static const char SINGULAR_TENDER_NAMES[][TENDER_STRING_LENGTH]
         "fifty dollar",
         "hundered dollar" };
 
-static const char PLURAL_TENDER_NAMES[][TENDER_STRING_LENGTH]
-    = { "pennies",
-        "nickels",
-        "dimes",
-        "quarters",
-        "dollars",
-        "five dollars",
-        "ten dollars",
-        "twenty dollars",
-        "fifty dollars",
-        "hundred dollars" };
-
-
-static const int TENDER_VALUES[]
+static const int DENOMINATION_VALUES[]
     = { 1,
         5,
         10,
@@ -59,15 +49,22 @@ static const int TENDER_VALUES[]
         1000,
         2000 };
 
-#define NUM_TENDER (sizeof(TENDER_VALUES) / sizeof(int))
+#define NUM_DENOMINATION (sizeof(DENOMINATION_VALUES) / sizeof(int))
+
+typedef struct {
+    int denomination[NUM_DENOMINATION];
+} Tender;
 
 int main_dev(int, char **);
 int parse_tender_string(const char *);
+void mkchange(Tender *, int);
+void render_tender(Tender *);
 
 int main(int argc, char **argv) {
 #ifdef DEV
     return main_dev(argc, argv);
 #endif
+
 }
 
 int main_dev(int argc, char **argv) {
@@ -75,12 +72,41 @@ int main_dev(int argc, char **argv) {
     char user_input[256];
     memset(user_input, 0, 256);
     while (true) {
-        printf("enter a currency value: ");
+        int user_total = 0;
+        Tender tender;
+        printf("Enter a currency value: ");
         scanf("%s", user_input);
-        printf("Converted: %d\n", parse_tender_string(user_input));
+        user_total = parse_tender_string(user_input);
+        if (user_total < 0) {
+            printf("Error in parse: %s\n", user_input);
+            continue;
+        }
+        mkchange(&tender, user_total);
+        render_tender(&tender);
     }
     return 0;
 }
+
+void render_tender(Tender *tender) {
+    size_t i = 0;
+    int total = 0;
+    for (; i < NUM_DENOMINATION; ++i) {
+        printf("%s = %d\n", SINGULAR_DENOMINATION_NAMES[i], tender->denomination[i]);
+        total += tender->denomination[i] * DENOMINATION_VALUES[i];
+    }
+
+    printf("Total change = %d.%02d\n", total / 100, total % 100);
+}
+
+void mkchange(Tender *change, int total) {
+    size_t i = NUM_DENOMINATION - 1;
+    for (; i > 0; --i) {
+        change->denomination[i] = (int)total / (int)DENOMINATION_VALUES[i];
+        total %= DENOMINATION_VALUES[i];
+    }
+    change->denomination[0] = total;
+}
+
 
 /* Parse a string containing a currency value. If the parse fails, returns -1.
  * Acceptable currency formats:
