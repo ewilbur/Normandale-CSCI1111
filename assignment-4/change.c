7caf7a414e2755626c20329936c16e9fb3d63a60
Evan Wilbur <ewilbur9@gmail.com>
2020-06-10 14:21:58 -0500

    Initial currency string validation

--- assignment-4/change.c
+++ assignment-4/change.c
@@ -19,7 +19,9 @@
 #include <stdbool.h>
 #include <math.h>
 #include <stdlib.h>
+#include <ctype.h>
 
+#define DEV
 #define TENDER_STRING_LENGTH 32
 
 static const char SINGULAR_TENDER_NAMES[][TENDER_STRING_LENGTH]
@@ -59,106 +61,42 @@ static const int TENDER_VALUES[]
 
 #define NUM_TENDER (sizeof(TENDER_VALUES) / sizeof(int))
 
-typedef struct {
-    int tender_values[NUM_TENDER];
-    int num_tender[NUM_TENDER];
-} Tender;
+int main_dev(int, char **);
+bool validate_currency_input(const char *);
+int read_currency_string(const char *);
 
-void printchange(Tender *);
-void mkchange(Tender *, int);
-void flush_buffer();
-Tender *mktender();
-void freetender(Tender *);
-int currency_to_cents(float);
+int main(int argc, char **argv) {
+#ifdef DEV
+    return main_dev(argc, argv);
+#endif
+}
 
-int main() {
-    float amount_paid = 0;
-    float amount_owed = 0;
+int main_dev(int argc, char **argv) {
+    return 0;
+}
 
-    int amount_paid_cent = 0;
-    int amount_owed_cent = 0;
-    int amount_in_change = 0;
+int read_currency_string(const char *tender) {
+}
 
-    int read_word_count = 0;
-
-    Tender *tender = mktender();
-
-    do {
-        printf("Enter the amount you owe: ");
-        read_word_count = scanf("%f", &amount_owed);
-        if (read_word_count <= 0) {
-            fprintf(stderr, "Invalid amount entered\n");
-            flush_buffer();
-        }
-        amount_owed_cent = currency_to_cents(amount_owed);
-    } while (read_word_count <= 0);
-
-    do {
-        printf("Enter the amount you paid: ");
-        read_word_count = scanf("%f", &amount_paid);
-        if (read_word_count <= 0) {
-            fprintf(stderr, "Invalid amount entered\n");
-            flush_buffer();
-        }
-        amount_paid_cent = currency_to_cents(amount_paid);
-    } while (read_word_count <= 0);
-
-    amount_in_change = amount_paid_cent - amount_owed_cent;
-    if (amount_in_change < 0) printf("Amount paid is less than amount owed. No change.\n");
-    else {
-        mkchange(tender, amount_in_change);
-        printchange(tender);
+bool validate_currency_input(const char *tender) {
+    size_t i = 0;
+    size_t j = 0;
+    /* Check that each char before the decimal is a digit */
+    while (tender[i] != '\0' && tender[i] != '.') {
+        if (!isdigit(tender[i])) return false;
+        ++i;
     }
-
-    freetender(tender);
-
-    flush_buffer();
-    printf("Enter 'q' to quit or any other key to go again: ");
-    if (getchar() != 'q') return main();
-    else return 0;
-}
-
-int currency_to_cents(float currency) {
-    return (int)floor(currency * 100.);
-}
-
-void printchange(Tender *tender) {
-    int i = 0;
-    int total_change = 0;
-    float actual_change = 0.;
-
-    for (; i < NUM_TENDER; ++i) {
-        printf("%s = %d\n",
-                tender->num_tender[i] != 1 ? PLURAL_TENDER_NAMES[i] : SINGULAR_TENDER_NAMES[i],
-                tender->num_tender[i]);
-        total_change += tender->num_tender[i] * tender->tender_values[i];
+    if (tender[i] == '\0') return false;
+    /* Otherwise, we have to make sure:
+     *  a) There are exactly two chars after the decimal
+     *  b) Each of those two chars are digits
+     */
+    for (j = 1; j < 3; ++j) {
+        if (tender[i + j] == '\0') return false;
+        if (!isdigit(tender[i + j])) return false;
     }
-
-    actual_change = ((float)total_change) / 100.0;
-    printf("Total change: %.2f\n", actual_change);
-}
-
-void mkchange(Tender *tender, int change) {
-    int i = NUM_TENDER - 1;
-
-    for (; i >= 0; --i) {
-        tender->num_tender[i] = (change / tender->tender_values[i]);
-        change -= tender->tender_values[i] * tender->num_tender[i];
-    }
-}
-
-Tender *mktender() {
-    Tender *tender = malloc(sizeof(Tender));
-    memmove(&tender->tender_values, TENDER_VALUES, sizeof(TENDER_VALUES));
-    memset(&tender->num_tender, 0, sizeof(NUM_TENDER));
-    return tender;
-}
-
-void freetender(Tender *tender) {
-    free(tender);
-}
-
-void flush_buffer() {
-    volatile char c;
-    while ((c = getchar()) != '\n' || c != EOF);
+    /* After checking two placed after the decimal point (see above) if we've
+     * reached the end of the input then the input is valid
+     */
+    return tender[i + j] == '\0';
 }
