6ed8250c893c2fcd379cf2029f9aceb8529de847
Evan Wilbur <ewilbur9@gmail.com>
2020-06-17 20:27:59 -0500

    Added prompt to top of question 2 and changed output formatting

--- assignment-6/question2.c
+++ assignment-6/question2.c
@@ -5,6 +5,7 @@ void page296_3(void) {
     size_t num_even = 0, num_odd = 0;
     int average_even = 0, average_odd = 0;
     int user_int;
+    printf("Enter characters, seperated by a newline. When you are done, enter '0'.\n");
     scanf("%d", &user_int);
     while (user_int != 0) {
         if (user_int % 2 == 0) {
@@ -18,10 +19,9 @@ void page296_3(void) {
         scanf("%d", &user_int);
     }
 
-    printf("     | Total entered | Average\n");
-    printf("------------------------------\n");
-    printf("Even | %7lu       | %f\n", num_even, (float)average_even / (float)num_even);
-    printf("Odd  | %7lu       | %f\n", num_odd, (float)average_odd / (float) num_odd);
-    while (getchar() != '\n');
+    printf("     | Total entered |   Average\n");
+    printf("---------------------------------\n");
+    printf("Even | %13lu | %lf\n", num_even, (double)average_even / (double)num_even);
+    printf("Odd  | %13lu | %lf\n", num_odd, (double)average_odd / (double) num_odd);
 }
 
