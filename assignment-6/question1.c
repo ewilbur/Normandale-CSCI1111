f4c56d0cc5c90098ff7c810e49782e7f4e69f8da
Evan Wilbur <ewilbur9@gmail.com>
2020-06-17 20:15:25 -0500

    Keeps line buffering on and removes buffer flush

--- assignment-6/question1.c
+++ assignment-6/question1.c
@@ -4,7 +4,6 @@
 void page296_1() {
     char ch;
     size_t num_newlines = 0, num_spaces = 0, num_else = 0;
-    setvbuf(stdin, NULL, _IONBF, 0);
     while ((ch = getchar()) != '#') {
         switch (ch) {
             case '\n': 
@@ -18,9 +17,7 @@ void page296_1() {
                 break;
         }
     }
-
     printf("%s|%s|%s\n", "Space Count", "Newline Count", "Other Characters");
     printf("------------------------------------------\n");
     printf("%5lu      |%7lu      |%9lu\n", num_spaces, num_newlines, num_else);
-    while (getchar() != '\n');
 }
