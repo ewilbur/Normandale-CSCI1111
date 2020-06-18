8237d68bc922d3e2c24aa6c5a0c09969f96fea21
Evan Wilbur <ewilbur9@gmail.com>
2020-06-17 20:36:11 -0500

    Fixed loop to efficiently print the primes

--- assignment-6/question4.c
+++ assignment-6/question4.c
@@ -11,6 +11,8 @@ void page296_9() {
     sieve(user_bound + 1);
 }
 
+/* Simple prime sieve. Runs in n*sqrt(n)
+ */
 void sieve(size_t bound) {
     unsigned char *primer = (unsigned char *)calloc(bound, sizeof(unsigned char));
     primer[0] = primer[1] = 1;
@@ -19,10 +21,11 @@ void sieve(size_t bound) {
         if (primer[i] == 0) {
             for (j = 2; j * i < bound; ++j)
                 primer[i * j] = 1;
+            printf("%lu\n", i);
         }
     }
 
-    for (i = 2; i < bound; ++i)
+    for (; i < bound; ++i)
         if (primer[i] == 0)
             printf("%lu\n", i);
     free(primer);
