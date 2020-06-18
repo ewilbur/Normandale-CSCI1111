32c2cec6cec712fb31ae1b2a721d916090a8a598
Evan Wilbur <ewilbur9@gmail.com>
2020-06-17 20:36:29 -0500

    Changed menu selection options

--- assignment-6/main.c
+++ assignment-6/main.c
@@ -22,16 +22,16 @@ int main() {
 run_io prompt_selection() {
     int user_selection = 0;
     printf("1 -- problem 1\n");
-    printf("3 -- problem 3\n");
-    printf("7 -- problem 7\n");
-    printf("9 -- problem 9\n\n");
+    printf("2 -- problem 3\n");
+    printf("3 -- problem 7\n");
+    printf("4 -- problem 9\n\n");
     printf("Which problem would you like to run? ");
     scanf("%d", &user_selection);
     switch (user_selection) {
         case 1: return page296_1;
-        case 3: return page296_3;
-        case 7: return page296_7;
-        case 9: return page296_9;
+        case 2: return page296_3;
+        case 3: return page296_7;
+        case 4: return page296_9;
         default:
             printf("Invalid selection! Pick again\n");
             return prompt_selection();
