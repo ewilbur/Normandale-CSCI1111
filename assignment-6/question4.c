bcbfa8bc25d262abf2884b80f18c0a263f19732d
Evan Wilbur <ewilbur9@gmail.com>
2020-06-18 11:18:33 -0500

    Fix bug on Windows causing undefined behavior when calling via function pointer

--- assignment-6/question4.c
+++ assignment-6/question4.c
@@ -4,10 +4,24 @@
 
 void sieve(size_t);
 
+
+/* | This is to fix a bug that took over three hours to find. Basically,
+ *   when reading a unsigned long long int from a function called via a
+ *   returned pointer, the variable gets underflowed and causes undefined
+ *   brehavior. It is fixed if you initialize the variable first to zero
+ *   or if you put in the correct formatting string for scanf. Here we do
+ *   both
+ */
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+const char format_string[] = "%llu";
+#else
+const char format_string[] = "%lu";
+#endif
+
 void page296_9() {
-    size_t user_bound;
+    size_t user_bound = 0;
     printf("Enter an upper bound: ");
-    scanf("%lu", &user_bound);
+    scanf(format_string, &user_bound);
     sieve(user_bound + 1);
 }
 
@@ -22,12 +36,15 @@ void sieve(size_t bound) {
         if (primer[i] == 0) {
             for (j = 2; j * i < bound; ++j)
                 primer[i * j] = 1;
-            printf("%lu\n", i);
+            printf(format_string, i);
+            printf("\n");
         }
     }
 
     for (; i < bound; ++i)
-        if (primer[i] == 0)
-            printf("%lu\n", i);
+        if (primer[i] == 0) {
+            printf(format_string, i);
+            printf("\n");
+        }
     free(primer);
 }
